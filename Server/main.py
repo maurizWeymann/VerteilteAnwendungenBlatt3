0257732f8c1602f015d1fda6c835058ae8e83a89
TwoFingers Catering <s0528279@htw-berlin.de>
2022-06-30 00:53:08 +0200

    API I

--- Server/main.py
+++ Server/main.py
@@ -6,6 +6,7 @@ Created on Tue Jun 28 14:25:03 2022
 """
 
 from cmath import log
+from distutils.log import error
 from hashlib import new
 from imp import reload
 from pickletools import read_uint1
@@ -20,7 +21,7 @@ import random
 import time
 
 from yaml import load
-from user import User 
+#from user import User 
 import json
 
 app = FastAPI(
@@ -30,33 +31,62 @@ app = FastAPI(
     version="1.0",
     openapi_url="/api/v1/openapi.json"
 )
-def read_from_file(filename):
-    with open(filename, "r") as read_file:
-        contain = json.load(read_file)
-        #print(contain)
-        return contain
+
+data = []
+names = []
+user_df = {}
+
+user_df = pd.DataFrame(data, columns = [
+    'user_name',
+    'password',
+    'score',
+    ])
+
+
+user_df = user_df.append({"user_name":"mo", "password":"xyz123", "score":0}, ignore_index=True) 
 
 '''
 I Nutzer anlegen und speichern 
 '''
-@app.put("/api/v1/users/add", status_code=201)
-async def create_user(user_name):
-    return
-
+@app.put("/api/v1/users/add")
+async def create_user(user_name: str, password: str, response: Response):
+    try:
+        if user_name not in list(user_df["user_name"]):
+            user_df.loc[len(user_df)] = [user_name, password, 0]
+            response.status_code = status.HTTP_201_CREATED
+            return "new user created"
+        else:
+            response.status_code = status.HTTP_403_FORBIDDEN
+            print(user_df)
+            return "user already in use"
+    except:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    
 '''
 II Nutzer anmelden und Spiel starten 
 '''
-@app.put("/api/v1/users/login", status_code=200)
-async def login_user(user_name):
-    return
+@app.put("/api/v1/users/login")
+async def login_user(user_name: str, password: str, response: Response):
+    try:
+        if user_name not in list(user_df["user_name"]):
+            user_df.loc[len(user_df)] = [user_name, password, 0]
+            response.status_code = status.HTTP_201_CREATED
+            return "new user created"
+        else:
+            response.status_code = status.HTTP_403_FORBIDDEN
+            print(user_df)
+            return "user already in use"
+    except:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 
 '''
 III Fragen abrufen 
 '''
-@app.get("/api/v1/questions", status_code=200)
-async def load_questions():
-    #data = read_from_file("I-PUT-users_add.json")
-    return "I-PUT-users_add.json"
+@app.get("/api/v1/questions")
+async def load_questions(response:Response):
+    response.status_code = status.HTTP_200_OK
+    return user_df.to_dict()
+    
 '''
 IV Fragen hochladen und speichern
 '''
@@ -64,7 +94,7 @@ IV Fragen hochladen und speichern
 async def create_question():
     return 
 
-
+#user_df.loc[len(user_df),"password"] = [password]
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=22222, log_level='debug', reload= True)
+    uvicorn.run("main:app", host="0.0.0.0", port=22224, log_level='debug')
