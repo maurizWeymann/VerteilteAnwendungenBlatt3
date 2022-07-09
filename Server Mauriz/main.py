9495108e803a09f53d4bb3093c4cfd03056daa22
TwoFingers Catering <s0528279@htw-berlin.de>
2022-07-09 14:29:59 +0200

    fixed csv import and export - list trouble

--- Server Mauriz/main.py
+++ Server Mauriz/main.py
@@ -18,7 +18,9 @@ import time
 from typing import List
 from yaml import load
 import json
+import os
 from mydata import User, PutUser, Question, PutQuestion, PutAnswer, ScoringList, MyUsers 
+from ast import literal_eval
 
 app = FastAPI(
     docs_url="/api/v1/docs",
@@ -53,9 +55,6 @@ question_df = pd.DataFrame(data, columns = [
     'options',
     ])
 
-
-user_df = user_df.append({"name":"mo", "password":"xyz123", "score":0}, ignore_index=True) 
-user_df = user_df.append({"name":"mau", "password":"z1", "score":0}, ignore_index=True)
 #############################################
 question_list.append({
     "question_id": 1,
@@ -64,59 +63,38 @@ question_list.append({
     "options":["Thomas","Antonio"],
 }) 
 #############################################
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "Who is called the god of lightning in Avengers?",
-    "answer" : "Thor",
-    "options":["Thorsten","Torben"],
-}, ignore_index=True)
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "Who carries a shield of American flag theme in Avengers?",
-    "options":["Steve", "Roger"],
-    "answer" : "Captain America"
-}, ignore_index=True)
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "What is the first name of Iron Man?",
-    "options":["Thomas","Antonio"],
-    "answer" : "Tony"
-}, ignore_index=True)
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "Which avenger is green in color?",
-    "options":["Spiderman","Vision"],
-    "answer" : "Hulk"
-}, ignore_index=True)
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "Which avenger can change it's size?",
-    "options":["ChangeMan", "SizeMan"],
-    "answer" : "AntMan"
-}, ignore_index=True)
-question_df = question_df.append({
-    "id":len(question_df)+1,
-    "question" : "Which Avenger is red in color and has mind stone?",
-    "options":["MindMan","RedMan"],
-    "answer" : "Vision"
-}, ignore_index=True)
+
 
 #question_list.append( {"question_id" : question_df.loc[0, "id"], "question" : question_df.loc[0, "question"], "options" : question_df.loc[0, "options"] + [(question_df.loc[0, "answer"])] } )
 for index, user in user_df.iterrows() :
     scoring_list.append({ "name" : user_df.loc[index, "name"], "score" : user_df.loc[index, "score"] })
 
 
-print( question_list.append( {
-    "question_id" : question_df.loc[0, "id"],
-    "question" : question_df.loc[0, "question"]
-    } )
-)
-
-def save_questions_to_csv():
-    question_df.to_csv('question.csv',index=False)
-    
+'''
+Handle Question Data
+'''
 def load_questions_from_csv():
-    question_df = pd.read_csv('question.csv')
+    return pd.read_csv('question.csv', delimiter=";",converters={'options': literal_eval})
+
+def save_questions_to_csv(question_df):
+    question_df.to_csv('question.csv',index=False, sep=";")
+'''
+Handle User Data
+'''
+def load_users_from_csv():
+    return pd.read_csv('user.csv')
+
+def save_user_to_csv(user_df):
+    user_df.to_csv('user.csv',index=False)
+
+
+question_df = question_df.append({
+    "id":len(question_df)+1,
+    "question" : "Who is called the god of lightning in Avengers?",
+    "answer" : "Thor",
+    "options":["Thorsten","Torben"],
+}, ignore_index=True)
+
 
 '''
 I Nutzer anlegen und speichern 
@@ -124,13 +102,23 @@ I Nutzer anlegen und speichern
 @app.put("/api/v1/users/add")
 async def create_user(user: PutUser, response: Response):
     try:
+        try:
+            user_df = load_users_from_csv()
+        except:
+            print("unable to load data from csv file") 
+        
         if user.name not in list(user_df["name"]):
             user_df.loc[len(user_df)] = [user.name, user.password, 0]
-            response.status_code = status.HTTP_201_CREATED
-            return "new user created"
+            
+            try:
+                save_user_to_csv(user_df)
+                response.status_code = status.HTTP_201_CREATED
+                return "new user created"
+            except:
+                print("unable to save user data to csv file") 
+            
         else:
             response.status_code = status.HTTP_403_FORBIDDEN
-            print(user_df)
             return "user already in use"
     except:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -142,6 +130,11 @@ II Nutzer anmelden und Spiel starten
 @app.put("/api/v1/users/login")
 async def login_user(user: PutUser, response: Response):
     try:
+        try:
+            user_df = load_users_from_csv()
+        except:
+            print("unable to load data from csv file") 
+
         if user.name not in list(user_df["name"]):
             response.status_code = status.HTTP_403_FORBIDDEN
             return "bad credentials"
@@ -150,7 +143,12 @@ async def login_user(user: PutUser, response: Response):
             df = user_df.set_index("name", drop = False)
             if user.password in df.loc[user.name]["password"]:
                 response.status_code = status.HTTP_202_ACCEPTED
-                return "successful logged in "
+                try:
+                    #initiates question data frame 
+                    question_df = load_questions_from_csv()
+                    return "successful logged in"
+                except:
+                    print("unable to load data from csv file") 
             else:
                 response.status_code = status.HTTP_401_UNAUTHORIZED
                 return "wrong combination"
@@ -162,22 +160,42 @@ async def login_user(user: PutUser, response: Response):
 III Fragen abrufen 
 '''
 @app.get("/api/v1/questions/{number_of_questions}", response_model = List[Question])
-async def load_questions(*,number_of_questions: int = Path(title="The ID of the item to get", ge=1, le=len(question_df)), response:Response):
-    response.status_code = status.HTTP_200_OK
-    question_list = []
-    for sample in random.sample(range(0,len(question_df)),number_of_questions):
-        question_list.append( {"question_id" : question_df.loc[sample, "id"], "question" : question_df.loc[sample, "question"], "options" : question_df.loc[sample, "options"] + [(question_df.loc[sample, "answer"])] } )
-    return question_list    
+async def load_questions(*,number_of_questions: int = Path(title="The ID of the item to get", ge=1, le=len(load_questions_from_csv())), response:Response):
+    try:
+        question_df = load_questions_from_csv()
+    except:
+        print("unable to load data from csv file")
+    
+    try:
+        question_list = []
+        for sample in random.sample(range(0,len(question_df)),number_of_questions):
+            question_list.append( {"question_id" : question_df.loc[sample, "id"], "question" : question_df.loc[sample, "question"], "options" : question_df.loc[sample, "options"] + [question_df.loc[sample, "answer"]]  } )
+        print(question_list)
+        response.status_code = status.HTTP_200_OK
+        return question_list
+    
+    except:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return question_list
+
 '''
 IV Fragen hochladen und speichern
 '''
 @app.put("/api/v1/questions/add")
 async def create_question(question: PutQuestion, response: Response):
     try:
+        try:
+            question_df = load_questions_from_csv()
+        except:
+            print("unable to load data from csv file")
         if question.question not in list(question_df["question"]):
             question_df.loc[len(question_df)] = [ max(list(question_df["id"]))+1, question.question, question.answer, question.options]
             response.status_code = status.HTTP_201_CREATED
-            return "new question created"
+            try:
+                save_questions_to_csv(question_df)
+            except:
+                print("unable to save data to csv file")
+            return "new question created" 
         else:
             response.status_code = status.HTTP_403_FORBIDDEN
             return "already in place"
@@ -198,6 +216,10 @@ VI Punkteliste und Highscore aller Spieler abfragen
 @app.get("/api/v1/users/highscore", response_model=ScoringList)
 async def return_highscores(response: Response):
     try:
+        try:
+            user_df = load_users_from_csv()
+        except:
+            print("unable to load data from csv file")            
         scoring_list = []
         for index, user in user_df.iterrows() :
             scoring_list.append({ "name" : user_df.loc[index, "name"], "score" : user_df.loc[index, "score"] })
